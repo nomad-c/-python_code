39222e64b1f7eb3b2bf25d23f17e24caf25450fc
nomad-c <piotr.c.piotrowski@gmail.com>
2021-12-29 13:33:46 +0100

    Update papkamnoz2.py

--- papkamnoz2.py
+++ papkamnoz2.py
@@ -1,61 +1,109 @@
 # papier, kamień nożyczki z tablicą i funkcją 29.12.2021 Czarny
+# nowości to random.choice(listy)
+# wykorzystanie zmiennej gra do możliwych wyników
+# do trzech razy sztuka z wykorzystaniem for i range
 import random
 
-win, lose, remis, wybor, wybkom = 0, 0, 0, "p", None
+wybor, wybkom = None, None
 # wybor - wybór człowieka
-# wybór jest równe 'p' żeby od czegoś zacząć
-# ale to nie ma znaczenia bo potem i tak mu ustawiam
 # wybkom - wybór komputera
+# inicjalizuje mimo, że nie muszę ale chodzi o opisanie
+status = [0, 0, 0]  # tu ukryłem wygrane, przegrane i remisy
 gra = ["p", "k", "n"]
 
-while wybor in gra:  # jak wybierzesz coś innego niż poprawnie to wyjdziemy z pętli
-    print("Papier, Kamień, Nożyczki")
-    print("Wygrałeś " + str(win) + " przegrałeś " + str(lose) + " remis " + str(remis))
-    wybor = input("Wybierz p,k,n albo q czy coś innego, żeby opuścić grę\n:")
-    if wybor == "p":
-        print("Wybrałeś PAPIER")
-    if wybor == "k":
-        print("Wybrałeś KAMIEŃ")
-    if wybor == "n":
-        print("Wybrałeś NOŻYCZKI")
-    if (wybor != "p") and (wybor != "k") and (wybor != "n"):
-        print("koniec")
-        wybor = "q"
-        break
-    wybkom = random.randint(gra)  # Losowanie wartości od 0 do 2 (0,1,2)
-    # tak naprawdę są trzy przypadki
-    # jakbym rozkładał każdy z każdym tak jak w książce
-    # komputer miałby co najmniej 3 razy więcej roboty
-    if wybkom == "p":  # przekładam to na literkę, żeby mi się nie pozajączkowało
-        print("Komputer wybrał PAPIER")
-        if wybor == "n":
-            win += 1
-            print("Wygrałeś")
-        elif wybor == "k":
-            lose += 1
-            print("Przegrałeś")
-        else:
-            remis += 1
-            print("Remis")
-    if wybkom == "k":
-        print("Komputer wybrał KAMIEŃ")
+
+def podsumowanie():  # zrobiłem taką funkcję bo to dwa razy robię
+    print(
+        "Wygrałeś "
+        + str(status[0])
+        + " przegrałeś "
+        + str(status[1])
+        + " remis "
+        + str(status[2])
+    )  # podsumowanie
+    if status[0] > status[1]:
+        print("Tak generalnie to wygrałeś")
+        print("Gratulacje !!!")
+    elif status[0] < status[1]:
+        print("Tak generalnie to przegrałeś")
+        print("Dupa")
+    else:
+        print("No remis jest")
+
+
+###############################################################
+# koniec funkcji podsumowanie
+####################################################
+#  zaczynamy funkcję - Cała gra
+def calagra(iteracje):
+    for i in range(iteracje):  # do iteracji razy sztuka
+        print(
+            "Wygrałeś "
+            + str(status[0])
+            + " przegrałeś "
+            + str(status[1])
+            + " remis "
+            + str(status[2])
+        )
+        wybor = input("Wybierz p,k,n albo cokolwiek innego, żeby opuścić grę\n:")
         if wybor == "p":
-            win += 1
-            print("Wygrałeś")
-        elif wybor == "n":
-            lose += 1
-            print("Przegrałeś")
-        else:
-            remis += 1
-            print("Remis")
-    if wybkom == "n":
-        print("Komputer wybrał NOŻYCZKI")
+            print("Ty wybrałeś PAPIER", end=" ")
+        # end=' ' żeby nie przeskakiwał do następnej linii
         if wybor == "k":
-            win += 1
-            print("Wygrałeś")
-        elif wybor == "p":
-            lose += 1
-            print("Przegrałeś")
-        else:
-            remis += 1
-            print("Remis")
+            print("Ty wybrałeś KAMIEŃ", end=" ")
+        if wybor == "n":
+            print("Ty wybrałeś NOŻYCZKI", end=" ")
+        if (wybor != "p") and (wybor != "k") and (wybor != "n"):
+            print("Trzeba było coś wybrać")
+            print("Przegrałeś walkowerem")
+            status[1] += 1
+            continue
+        wybkom = random.choice(gra)  # Losowanie wartości ze zbioru gra
+        # tak naprawdę są trzy przypadki
+        # jakbym rozkładał każdy z każdym tak jak w książce
+        # komputer miałby co najmniej 3 razy więcej roboty
+        if wybkom == "p":
+            print("a komputer wybrał PAPIER")
+            if wybor == "n":
+                status[0] += 1
+                print("Wygrałeś")
+            elif wybor == "k":
+                status[1] += 1
+                print("Przegrałeś")
+        if wybkom == "k":
+            print("a komputer wybrał KAMIEŃ")
+            if wybor == "p":
+                status[0] += 1
+                print("Wygrałeś")
+            elif wybor == "n":
+                status[1] += 1
+                print("Przegrałeś")
+        if wybkom == "n":
+            print("a komputer wybrał NOŻYCZKI")
+            if wybor == "k":
+                status[0] += 1
+                print("Wygrałeś")
+            elif wybor == "p":
+                status[1] += 1
+                print("Przegrałeś")
+        if wybkom == wybor:
+            status[2] += 1
+            print("Remis")  # bo remis jest wszędzie taki sam
+            # i nie trzeba tego trzy razy sprawdzać
+            # ale warunek musi być na końcu bo inaczej komunikaty są w złej kolejności
+
+
+# tu się zaczyna program
+wybor1 = "cokolwiek"
+# zmienna po to, żeby się zapytać czy gra dalej
+# nie mogłem wykorzystać ponownie wybor
+# bo n oznacza nożyczki
+
+while wybor1 != "n":
+    print("Do trzech razy sztuka")
+    print("Papier, Kamień, Nożyczki")
+    calagra(3)
+    podsumowanie()
+    wybor1 = input("Grasz ponownie ?(t/n):")
+podsumowanie()
+# koniec
