969fd4ed94cea57d8a2a0333f8ed716097d05793
nomad-c <piotr.c.piotrowski@gmail.com>
2022-01-10 16:01:36 +0100

    nowy rok

--- generator_hasel.py
+++ generator_hasel.py
@@ -6,7 +6,8 @@ import random
 # inicjalizacja tablic
 cyfry1 = "1 2 3 4 5 6 7 8 9 0"
 cyfry = cyfry1.split()
-duze1 = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
+duze1 = "A B C D E F G H I J K L M N P Q R S T U V W X Y Z"
+# nie ma dużego O
 duze = duze1.split()
 znaki1 = "! @ # $ % ^ & * ( ) - + [ ]"
 znaki = znaki1.split()
@@ -18,6 +19,7 @@ def generuj(ile):
     haslo, haslo1, haslo2 = "", "", ""
     if ile < 8:
         ile = 8
+    #  losujemy dużą, małą, cyfrę i znak
     haslo1 = (
         haslo1
         + " "
@@ -29,27 +31,24 @@ def generuj(ile):
         + " "
         + random.choice(male)
     )
+    # losujemy same znaki, żeby było łatwiej wpisywać
     for i in range(ile):
-        haslo2 = (
-            haslo2
-            + " "
-            + random.choice(cyfry)
-            + " "
-            + random.choice(duze)
-            + " "
-            + random.choice(znaki)
-            + " "
-            + random.choice(male)
-        )
-    haslo3 = haslo2.split()  # mam wylosowany zbiór znaków z których zrobię haslo
-    haslo4 = haslo1.split()
+        haslo2 = haslo2 + " " + random.choice(duze) + " " + random.choice(male)
+    haslo3 = haslo2.split()  # mam wylosowany zbiór znaków z których zrobię haslo - specjalnie mieszam, żeby więcej losować
+    haslo4 = haslo1.split()  # tu mam wylosowane cztery obowiązkowe
+    haslo5 = ""              # tu składam całość
     for i in range(4):
-        haslo = haslo + haslo4[i]
+        haslo5 = haslo5 + " " + haslo4[i]
+    haslo5 = haslo5 + " "
     for i in range(ile - 4):
-        haslo = haslo + haslo3[i]
+        haslo5 = haslo5 + " " + haslo3[i]
+    haslo6 = haslo5.split()
+    random.shuffle(haslo6)  # i teraz jeszcze raz mieszamy
+    for i in range(ile):
+        haslo = haslo + haslo6[i]
     return haslo
 
 
-print("enerator hasel")
+print("Generator hasel")
 ile1 = int(input("Ile potrzebujesz znakow ?"))
 print(generuj(ile1))
---
+++ szyfrowacz.py
@@ -0,0 +1,24 @@
+# konwerter tekstu / szyfrowacz 10.01.2022
+# jeszcze trochę...
+
+tabela = [
+        ['trzy', '3'],
+        ['tu', '2'],
+        ['ka', 'K'],
+        ['o', '0'],
+        ['e', '3'],
+        ['s', '5'],
+        ['5', '$'],
+        ['a', '4'],
+        ['b', '!6'],
+        ['i', '!'],
+        ['sto', '100'],
+        ['ku', 'Q'],
+        ['r', '4'],
+        ['h', '#'],
+        ['t', '7']
+        ]
+
+wpis = input('Wpisz swój tekst')
+for i = 1 in range(len(wpis))
+
